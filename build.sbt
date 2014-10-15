ec16de76249490e2d3e99f918c88dc06d85c50cf
alduk <Arsen.Kudla@gmail.com>
2014-10-15 11:59:36 +0300

    Removed conf, added execute to protocol

--- build.sbt
+++ build.sbt
@@ -2,7 +2,7 @@ name := "wcs-akka-api"
 
 version := "1.0"
 
-scalaVersion := "2.10.4"
+scalaVersion := "2.11.2"
 
 scalacOptions ++= Seq("-deprecation")
 
--- src/main/scala/com/sysiq/commerce/akka/api/RegistriesAPI.scala
+++ src/main/scala/com/sysiq/commerce/akka/api/RegistriesAPI.scala
@@ -1,8 +1,10 @@
 package com.sysiq.commerce.akka.api
 
 object RegistriesAPI {
-	case class CommandRegistryEntryV(key: String, interfaceName: String, className: String, storeId: Int)
-	case object Command
-	case object RegistriesList
-	case class CommandResponse(cache: scala.collection.mutable.Map[String, CommandRegistryEntryV])
+  case class CommandRegistryEntryV(key: String, interfaceName: String, className: String, storeId: Int)
+  case object Command
+  case object RegistriesList
+  case class CommandResponse(cache: scala.collection.mutable.Map[String, CommandRegistryEntryV])
+  case class Execute(script: String)
+  case class ExecutionResult(result: Any)
 }
